e9356ab8ad97472e2d37e7033cea96fcf1c06958
EC2 Default User <ec2-user@ip-172-31-27-231.ec2.internal>
2017-02-16 18:30:12 +0000

    bug fix

--- cookbooks/apachevirtual/recipes/default.rb
+++ cookbooks/apachevirtual/recipes/default.rb
@@ -5,28 +5,28 @@
 # Copyright 2017, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-cookbook_file "./Dockerfile" do
+directory "/apachevirtual/sites" do
+ mode "0755"
+end
+cookbook_file "/apachevirtual/Dockerfile" do
   source "Dockerfile"
   action :create
-end
-directory "./sites" do
- mode "0755"
 end
 node["apache"]["sites"].each do |site_name, site_data|
   document_root = "#{site_name}"
  
-  template "./sites/#{site_name}.conf" do
+  template "/apachevirtual/sites/#{site_name}.conf" do
    source "custom.erb"
    mode "0644"
    variables(:document_root => document_root,:port => site_data["port"])
   end
  
-  directory "./#{document_root}" do
+  directory "/apachevirtual/#{document_root}" do
    mode "0755"
    recursive true
   end
  
-  template "./#{document_root}/index.html" do
+  template "/apachevirtual/#{document_root}/index.html" do
    source "index.html.erb"
    mode "0644"
    variables(:site_name => site_name, :port => site_data["port"])
