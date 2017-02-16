584821f3c22db5fbb0799fe78bcbce7de279b1f4
EC2 Default User <ec2-user@ip-172-31-27-231.ec2.internal>
2017-02-16 17:05:57 +0000

    intial commit

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+package "httpd" do   
+   action :install 
+end 
+ 
+service "httpd" do   
+   action [ :enable, :start ] 
+end 
+ 
+cookbook_file "/var/www/html/index.html" do
+   source node["apache"]["indexfile"] 
+   mode "0644" 
+end
---
+++ cookbooks/apachevirtual/recipes/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: apachevirtual
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+cookbookfile "./Dockerfile" do
+  source "Dockerfile"
+  action :create
+done
+node["apache"]["sites"].each do |site_name, site_data|
+  document_root = "#{site_name}"
+ 
+  template "sites/#{site_name}.conf" do
+   source "custom.erb"
+   mode "0644"
+   variables(:document_root => document_root,:port => site_data["port"])
+  end
+ 
+  directory document_root do
+   mode "0755"
+   recursive true
+  end
+ 
+  template "#{document_root}/index.html" do
+   source "index.html.erb"
+   mode "0644"
+   variables(:site_name => site_name, :port => site_data["port"])
+  end
+end
