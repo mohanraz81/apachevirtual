7ee231e33dd461f37a43cd8659758113c56b8616
EC2 Default User <ec2-user@ip-172-31-27-231.ec2.internal>
2017-02-16 18:19:00 +0000

    bug fix

--- cookbooks/apachevirtual/recipes/default.rb
+++ cookbooks/apachevirtual/recipes/default.rb
@@ -15,18 +15,18 @@ end
 node["apache"]["sites"].each do |site_name, site_data|
   document_root = "#{site_name}"
  
-  template "sites/#{site_name}.conf" do
+  template "./sites/#{site_name}.conf" do
    source "custom.erb"
    mode "0644"
    variables(:document_root => document_root,:port => site_data["port"])
   end
  
-  directory document_root do
+  directory "./#{document_root}" do
    mode "0755"
    recursive true
   end
  
-  template "#{document_root}/index.html" do
+  template "./#{document_root}/index.html" do
    source "index.html.erb"
    mode "0644"
    variables(:site_name => site_name, :port => site_data["port"])
