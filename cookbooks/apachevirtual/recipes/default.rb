c0bd8f526a7a8fbdd74c2dbeafc745e1da52cc95
EC2 Default User <ec2-user@ip-172-31-27-231.ec2.internal>
2017-02-16 18:11:13 +0000

    bug fix

--- cookbooks/apachevirtual/recipes/default.rb
+++ cookbooks/apachevirtual/recipes/default.rb
@@ -9,6 +9,9 @@ cookbook_file "./Dockerfile" do
   source "Dockerfile"
   action :create
 end
+directory sites do
+ mode "0755"
+end
 node["apache"]["sites"].each do |site_name, site_data|
   document_root = "#{site_name}"
  
